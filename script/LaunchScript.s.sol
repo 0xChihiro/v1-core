4b634b00f97c251d966c5b1a46747c004d548d77
0xChihiro <kodawariproject04@gmail.com>
2026-06-05 18:20:01 -0500

    update launch script

--- script/LaunchScript.s.sol
+++ script/LaunchScript.s.sol
@@ -7,18 +7,17 @@ import {Controller} from "../src/Controller.sol";
 import {ControllerFactory} from "../src/factories/ControllerFactory.sol";
 import {CreationCodeStore} from "../src/factories/CreationCodeStore.sol";
 import {IControllerFactory} from "../src/interfaces/IControllerFactory.sol";
+import {ProtocolCollector} from "../src/ProtocolCollector.sol";
 import {Kernel} from "../src/Kernel.sol";
 import {Token} from "../src/Token.sol";
 import {Vault} from "../src/Vault.sol";
 
 contract LaunchScript is Script {
-    /// Not the actual controller factory address. Will be replaced once the factory is live.
-    // ControllerFactory constant factory = ControllerFactory(0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2);
-    /// Change admin address to the address you want the admin address to be.
-    address admin = address(1);
-
     function run() public {
-        vm.startBroadcast();
+        address admin = vm.envAddress("ADMIN_ADDRESS");
+        uint256 deployerPk = vm.envUint("DEPLOYER_PRIVATE_KEY");
+        vm.startBroadcast(deployerPk);
+        address protocolCollector = address(new ProtocolCollector(admin));
         IControllerFactory.CreationCodeStores memory codeStores = IControllerFactory.CreationCodeStores({
             controller: address(new CreationCodeStore(type(Controller).creationCode)),
             kernel: address(new CreationCodeStore(type(Kernel).creationCode)),
@@ -26,7 +25,7 @@ contract LaunchScript is Script {
             token: address(new CreationCodeStore(type(Token).creationCode))
         });
 
-        ControllerFactory factory = new ControllerFactory(address(42), codeStores);
+        ControllerFactory factory = new ControllerFactory(protocolCollector, codeStores);
 
         bytes32 salt = keccak256("enten.controller.launch");
 
@@ -34,8 +33,8 @@ contract LaunchScript is Script {
         IControllerFactory.LaunchConfig memory config = IControllerFactory.LaunchConfig({
             tokenName: "Enten",
             tokenSymbol: "ENTEN",
-            preMineAddress: address(0),
-            preMineAmount: 0,
+            preMineAddress: admin,
+            preMineAmount: 3_500_000e18,
             maxSupply: 10_000_000e18
         });
 
@@ -44,6 +43,7 @@ contract LaunchScript is Script {
 
         vm.stopBroadcast();
 
+        console.log(address(factory));
         console.log(deployments.controller);
         console.log(deployments.kernel);
         console.log(deployments.vault);
